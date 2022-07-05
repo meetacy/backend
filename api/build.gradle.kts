06446f0af0693f05e8014d8f9f6faad92411ede8
y9vad9 <y9vad9@gmail.com>
2022-07-05 12:41:21 +0300

    feat: added deploy task

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.util.GUtil.loadProperties
+
 plugins {
     id(Deps.Plugins.Configuration.Kotlin.Jvm)
     id(Deps.Plugins.Serialization.Id)
+    id(Deps.Plugins.Deploy.Id)
 }
 
 dependencies {
@@ -15,4 +18,30 @@ dependencies {
     testImplementation(Deps.Libs.Ktor.Client.Cio)
     testImplementation(Deps.Libs.Ktor.Client.SerializationJson)
     testImplementation(Deps.Libs.Ktor.Client.ContentNegotiation)
+}
+
+val service = "meetacy"
+
+val propertiesFile = rootProject.file("deploy.properties")
+
+deploy {
+    if (propertiesFile.exists()) {
+        ignore = false
+        val properties = loadProperties(propertiesFile)
+
+        host = properties.getProperty("host")
+        user = properties.getProperty("user")
+        password = properties.getProperty("password")
+        deployPath = properties.getProperty("deployPath")
+        knownHostsFile = properties.getProperty("knownHosts")
+
+        mainClass = "app.meetacy.backend.MainKt"
+        serviceName = service
+    } else {
+        ignore = true
+    }
+}
+
+application {
+    mainClass.set("app.meetacy.backend.MainKt")
 }
