168022b2e00693e4327b54359ccdfb16c25d8b46
y9Kap <wotblitz6allima4ok@gmail.com>
2022-07-08 23:44:00 +0300

    feat(close #18): Generate Token

--- api/src/main/kotlin/app/meetacy/backend/endpoint/auth/Routing.kt
+++ api/src/main/kotlin/app/meetacy/backend/endpoint/auth/Routing.kt
@@ -1,9 +1,11 @@
 package app.meetacy.backend.endpoint.auth
 
 import app.meetacy.backend.endpoint.auth.email.email
+import app.meetacy.backend.endpoint.auth.generate.getToken
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.route
 
 fun Route.auth() = route("/auth") {
     email()
+    getToken()
 }
