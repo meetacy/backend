175744bdc963249c38f027da59134a7140fd3e13
tsyganiimaksym <mixmax4559@gmail.com>
2022-08-08 22:34:45 +0300

    feat(#25): addFriendUsecase was added

---
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/friends/AddFriendUsecase.kt
@@ -0,0 +1,40 @@
+package app.meetacy.backend.usecase.friends
+
+import app.meetacy.backend.types.AccessHash
+import app.meetacy.backend.types.AccessToken
+import app.meetacy.backend.types.UserId
+import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.usecase.types.GetUsersViewsRepository
+import app.meetacy.backend.usecase.types.authorize
+
+
+class AddFriendUsecase(
+    private val authRepository: AuthRepository,
+    private val getUsersViewsRepository: GetUsersViewsRepository,
+    private val storage: Storage
+) {
+    suspend fun addFriendUsecase(
+        accessToken: AccessToken,
+        friendId: UserId,
+        friendAccessHash: AccessHash
+    ): Result {
+        val userId = authRepository.authorize(accessToken) { return Result.InvalidToken }
+        val friend = getUsersViewsRepository.getUsersViewsOrNull(userId, listOf(friendId))
+            .first()
+            ?: return Result.FriendNotFound
+        if (friend.accessHash != friendAccessHash) return Result.FriendNotFound
+
+        storage.addFriend(friendId)
+        return Result.Success
+    }
+    sealed interface Result {
+        object Success : Result
+        object InvalidToken : Result
+        object FriendNotFound : Result
+    }
+    interface Storage {
+        suspend fun addFriend(
+            friendId: UserId
+        )
+    }
+}
