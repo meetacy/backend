2cc391242c2712a11fd31b1b21b4cc0f606659a6
tsyganiimaksym <mixmax4559@gmail.com>
2022-10-07 16:25:23 +0300

    feat(close #46): download endpoint was created

--- endpoints/src/main/kotlin/app/meetacy/backend/endpoint/files/download/Routing.kt
+++ endpoints/src/main/kotlin/app/meetacy/backend/endpoint/files/download/Routing.kt
@@ -1,49 +1,49 @@
 package app.meetacy.backend.endpoint.files.download
 
 
-import app.meetacy.backend.types.FileId
 import app.meetacy.backend.types.FileIdentity
-import app.meetacy.backend.types.serialization.FileIdentitySerializable
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 import java.io.File
 
-@Serializable
-data class DownloadParams(
-    val fileIdentity: FileIdentitySerializable
+sealed interface GetFileResult {
+    class Success(val file: File) : GetFileResult
+    object InvalidIdentity : GetFileResult
+}
+
+class DownloadFileResponse(
+    val status: Boolean,
+    val errorCode: Int?,
+    val errorMessage: String?,
 )
-
-sealed interface S {}
-
 interface GetFileRepository {
-    suspend fun getFile (fileIdentity: FileIdentity): FileId
+    suspend fun getFile (fileIdentity: FileIdentity): GetFileResult
 }
 
 fun Route.download(getFileRepository: GetFileRepository) = get("/download") {
     val fileIdentity = call.receive<FileIdentity>()
-    val pathFile = getFileRepository.getFile(fileIdentity)
-    val file = File("D:/y9Kap/Downloads/Documents/$pathFile")
-    call.response.header(
-        HttpHeaders.ContentDisposition,
-        ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "$pathFile")
-            .toString()
-    )
-    call.respondFile(file)
-}
-fun Route.download() = get("/download") {
-    val file = File("D:/y9Kap/Downloads/Documents/Untitled.pdf")
-    call.response.header(
-        HttpHeaders.ContentDisposition,
-        ContentDisposition.Attachment.withParameter(
-            ContentDisposition.Parameters.Size,
-            "Untitled.pdf"
+    when(val result = getFileRepository.getFile(fileIdentity)) {
+        GetFileResult.InvalidIdentity -> call.respond(
+            DownloadFileResponse(
+                status = false,
+                errorCode = 1,
+                errorMessage = "Please provide a valid identity"
+            )
         )
-            .toString()
-    )
-    call.respondFile(file)
+        is GetFileResult.Success -> {
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(
+                    ContentDisposition.Parameters.Size, "${result.file.length()}"
+                ).toString()
+            )
+
+            call.respondFile(result.file)
+        }
+    }
+
 }
 
