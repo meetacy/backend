e147180db11902d6aff124c0f79998cff3de6456
y9Kap <wotblitz6allima4ok@gmail.com>
2023-02-11 17:04:46 +0300

    Revert "Revert "feat(#49): check utf8 string for generate token was added""
    This reverts commit b74c1135a6a1d358eae0f27935d625ee592a377f.

--- usecase/endpoints-integration/src/main/kotlin/app/meetacy/backend/usecase/integration/auth/UsecaseTokenGenerateRepository.kt
+++ usecase/endpoints-integration/src/main/kotlin/app/meetacy/backend/usecase/integration/auth/UsecaseTokenGenerateRepository.kt
@@ -1,10 +1,16 @@
 package app.meetacy.backend.usecase.integration.auth
 
 import app.meetacy.backend.endpoint.auth.generate.TokenGenerateRepository
-import app.meetacy.backend.types.AccessIdentity
+import app.meetacy.backend.endpoint.auth.generate.TokenGenerateResult
 import app.meetacy.backend.usecase.auth.GenerateTokenUsecase
 
 class UsecaseTokenGenerateRepository(private val usecase: GenerateTokenUsecase): TokenGenerateRepository {
-    override suspend fun generateToken(nickname: String): AccessIdentity =
-        usecase.generateToken(nickname)
+    override suspend fun generateToken(nickname: String): TokenGenerateResult {
+        return when(val result = usecase.generateToken(nickname)) {
+            GenerateTokenUsecase.Result.InvalidUtf8String ->
+                TokenGenerateResult.InvalidUtf8String
+            is GenerateTokenUsecase.Result.Success ->
+                TokenGenerateResult.Success(result.accessIdentity)
+        }
+    }
 }
