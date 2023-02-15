3e1ce7762d05fc728f3c97c2048a8350df75f421
y9Kap <wotblitz6allima4ok@gmail.com>
2023-02-15 22:55:51 +0300

    fix(#68): the file path for saving files to the server has been changed

--- application/src/main/kotlin/app/meetacy/backend/Main.kt
+++ application/src/main/kotlin/app/meetacy/backend/Main.kt
@@ -2,13 +2,17 @@ package app.meetacy.backend
 
 import app.meetacy.backend.infrastructure.startMockEndpoints
 import org.jetbrains.exposed.sql.Database
+import java.io.File
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
     val databaseUrl = System.getenv("DATABASE_URL") ?: error("Please provide a database url")
     val databaseUser = System.getenv("DATABASE_USER") ?: ""
     val databasePassword = System.getenv("DATABASE_PASSWORD") ?: ""
-    val filesBasePath = System.getenv("FILES_BASE_PATH") ?: "files"
+    val filesBasePath = System.getenv("FILES_BASE_PATH") ?: File(
+        /* parent = */ System.getenv("user.dir"),
+        /* child = */ "files"
+    ).apply { mkdirs() }.absolutePath
     val filesSizeLimit = System.getenv("FILES_SIZE_LIMIT")?.toLongOrNull() ?: (100L * 1024 * 1024)
     val database = Database.connect(
         databaseUrl,
