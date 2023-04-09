406eec4c13ac99aa5285f24297a4f0d3c19dbb83
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-04-09 15:00:41 +0300

    fix: Date parser fixed

--- types/src/main/kotlin/app/meetacy/backend/types/datetime/Date.kt
+++ types/src/main/kotlin/app/meetacy/backend/types/datetime/Date.kt
@@ -3,20 +3,23 @@ package app.meetacy.backend.types.datetime
 import app.meetacy.backend.types.annotation.UnsafeConstructor
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.time.Instant as JavaInstant
 import java.time.LocalDate as JavaLocalDate
 import java.util.Date as JavaDate
 
-private val iso8601DateFormat = SimpleDateFormat("yyyy-MM-dd").apply {
-    timeZone = TimeZone.getTimeZone("UTC")
-}
-
 @JvmInline
 value class Date @UnsafeConstructor constructor(val iso8601: String) {
 
-    val atStartOfDay: DateTime get() = iso8601DateFormat.parse(iso8601).meetacyDateTime
+    val atStartOfDay: DateTime get() = javaLocalDate
+        .atStartOfDay()
+        .atOffset(ZoneOffset.UTC)
+        .toInstant()
+        .meetacyDateTime
+
     val javaLocalDate: JavaLocalDate get() = JavaLocalDate.parse(iso8601)
 
     @OptIn(UnsafeConstructor::class)
@@ -25,18 +28,18 @@ value class Date @UnsafeConstructor constructor(val iso8601: String) {
         fun parse(iso8601: String): Date = parseOrNull(iso8601)
             ?: error("Given string '$iso8601' is not in iso8601 format for date without time")
         fun parseOrNull(iso8601: String): Date? = try {
-            iso8601DateFormat.parse(iso8601)
+            LocalDate.parse(iso8601)
             Date(iso8601)
-        } catch (_: ParseException) {
+        } catch (_: Throwable) {
             null
         }
     }
 }
 
-@OptIn(UnsafeConstructor::class)
-val JavaDate.meetacyDate: Date get() = Date(iso8601DateFormat.format(this))
+val JavaDate.meetacyDate: Date get() = toInstant().meetacyDate
 
-val JavaInstant.meetacyDate: Date get() = JavaDate(toEpochMilli()).meetacyDate
+@OptIn(UnsafeConstructor::class)
+val JavaInstant.meetacyDate: Date get() = Date(DateTimeFormatter.ISO_DATE.format(this))
 
 @OptIn(UnsafeConstructor::class)
 val JavaLocalDate.meetacyDate: Date get() = Date("$this")
--- types/src/main/kotlin/app/meetacy/backend/types/datetime/DateTime.kt
+++ types/src/main/kotlin/app/meetacy/backend/types/datetime/DateTime.kt
@@ -3,21 +3,20 @@ package app.meetacy.backend.types.datetime
 import app.meetacy.backend.types.annotation.UnsafeConstructor
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.time.Instant as JavaInstant
 import java.util.Date as JavaDate
 
-private val iso8601DateTimeFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").apply {
-    timeZone = TimeZone.getTimeZone("UTC")
-}
+private val dateTimeRegex = Regex("""\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}}Z""")
 
 @JvmInline
 value class DateTime @UnsafeConstructor constructor(val iso8601: String) {
     val date: Date get() = javaDate.meetacyDate
 
-    val javaInstant: JavaInstant get() = javaDate.toInstant()
+    val javaInstant: JavaInstant get() = JavaInstant.parse(iso8601)
 
-    val javaDate: JavaDate get() = iso8601DateTimeFormat.parse(iso8601)
+    val javaDate: JavaDate get() = JavaDate(javaInstant.toEpochMilli())
 
     @OptIn(UnsafeConstructor::class)
     companion object {
@@ -25,15 +24,16 @@ value class DateTime @UnsafeConstructor constructor(val iso8601: String) {
         fun parse(iso8601: String): DateTime = parseOrNull(iso8601)
             ?: error("Given string '$iso8601' is not in iso8601 format for date-time")
         fun parseOrNull(iso8601: String): DateTime? = try {
-            iso8601DateTimeFormat.parse(iso8601)
+            JavaInstant.parse(iso8601)
+            dateTimeRegex.matches(iso8601) || throw IllegalStateException()
             DateTime(iso8601)
-        } catch (_: ParseException) {
+        } catch (_: Throwable) {
             null
         }
     }
 }
 
-@OptIn(UnsafeConstructor::class)
-val JavaDate.meetacyDateTime: DateTime get() = DateTime(iso8601DateTimeFormat.format(this))
+val JavaDate.meetacyDateTime: DateTime get() = toInstant().meetacyDateTime
 
-val JavaInstant.meetacyDateTime: DateTime get() = JavaDate(toEpochMilli()).meetacyDateTime
+@OptIn(UnsafeConstructor::class)
+val JavaInstant.meetacyDateTime: DateTime get() = DateTime("$this")
