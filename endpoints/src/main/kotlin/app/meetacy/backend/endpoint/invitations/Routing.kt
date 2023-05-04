862b9161f3dcffba57655df45f8273155e6e1b9f
bpavuk <boopentiy@gmail.com>
2023-05-04 10:44:40 +0300

    feat(#61): linked new endpoints to invitationsRouting

--- endpoints/src/main/kotlin/app/meetacy/backend/endpoint/invitations/Routing.kt
+++ endpoints/src/main/kotlin/app/meetacy/backend/endpoint/invitations/Routing.kt
@@ -1,8 +1,10 @@
 package app.meetacy.backend.endpoint.invitations
 
+import app.meetacy.backend.endpoint.invitations.accept.invitationAcceptRouting
 import app.meetacy.backend.endpoint.invitations.crud.crudInvitationRouting
 import io.ktor.server.routing.*
 
 fun Route.invitationsRouting() = route("/invitations") {
     crudInvitationRouting()
+    invitationAcceptRouting()
 }
