6d504f105f4e990ba9699a0bb76573df3fd57e36
bpavuk <boopentiy@gmail.com>
2023-05-06 18:27:55 +0300

    WIP(#61): Added adapter between ReadInvitationUsecase and the database

---
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/read/ReadInvitationUsecase.kt
@@ -0,0 +1,63 @@
+package app.meetacy.backend.database.integration.invitation.read
+
+import app.meetacy.backend.database.invitations.InvitationsTable
+import app.meetacy.backend.database.types.DatabaseInvitation
+import app.meetacy.backend.database.users.UsersTable
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.invitations.read.ReadInvitationUsecase
+import app.meetacy.backend.usecase.invitations.read.ReadInvitationUsecase.Storage.DatabaseResult
+import app.meetacy.backend.usecase.types.Invitation
+import org.jetbrains.exposed.sql.Database
+
+
+class DatabaseReadInvitationStorage(db: Database): ReadInvitationUsecase.Storage {
+    private val usersTable = UsersTable(db)
+    private val invitationsTable = InvitationsTable(db)
+
+    override suspend fun UserId.getInvitations(): DatabaseResult {
+        if (usersTable.getUsersOrNull(listOf(this)).isEmpty()) return DatabaseResult.UserNotValid
+
+        val invitations = invitationsTable.getInvitations(invitedUserId = this)
+        return DatabaseResult.Success(invitations.map { it.toInvitation() })
+    }
+
+    override suspend fun UserId.getInvitations(from: List<UserId>): DatabaseResult {
+        if (usersTable.getUsersOrNull(listOf(this)).isEmpty()) return DatabaseResult.UserNotValid
+
+        val invitations = invitationsTable
+            .getInvitations(invitedUserId = this, invitorUserIdsList = from)
+            .filter { it.invitorUserId in from }
+
+        val retrievedFrom = invitations.map { it.invitorUserId }.toSet()
+
+        return if (retrievedFrom != from.toSet()) {
+            DatabaseResult.UsersNotFound
+        } else {
+            DatabaseResult.Success(invitations.map { it.toInvitation() })
+        }
+    }
+
+    override suspend fun UserId.getInvitations(ids: List<InvitationId>): DatabaseResult {
+        if (usersTable.getUsersOrNull(listOf(this)).isEmpty()) return DatabaseResult.UserNotValid
+
+        val invitations = invitationsTable
+            .getInvitations(this, ids)
+
+        return if (invitations.map { it.id }.toSet() != ids) {
+            DatabaseResult.InvitationsNotFound
+        } else {
+            DatabaseResult.Success(invitations.map { it.toInvitation() })
+        }
+    }
+
+    private fun DatabaseInvitation.toInvitation() = Invitation(
+        identity,
+        expiryDate,
+        invitedUserId,
+        invitorUserId,
+        meeting,
+        title,
+        description
+    )
+}
