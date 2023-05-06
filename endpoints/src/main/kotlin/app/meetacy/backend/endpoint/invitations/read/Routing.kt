fe130a28b97600bd241f965554b9ad5285506bed
bpavuk <boopentiy@gmail.com>
2023-05-06 20:36:21 +0300

    feat(#61): Implemented "/invitations/get" endpoint

--- endpoints/src/main/kotlin/app/meetacy/backend/endpoint/invitations/read/Routing.kt
+++ endpoints/src/main/kotlin/app/meetacy/backend/endpoint/invitations/read/Routing.kt
@@ -1,49 +1,60 @@
 package app.meetacy.backend.endpoint.invitations.read
 
-import app.meetacy.backend.endpoint.invitations.InvitationsGetDependencies
+import app.meetacy.backend.endpoint.ktor.Failure
+import app.meetacy.backend.endpoint.ktor.respondFailure
+import app.meetacy.backend.endpoint.ktor.respondSuccess
 import app.meetacy.backend.endpoint.types.Invitation
-import io.ktor.http.*
+import app.meetacy.backend.types.serialization.access.AccessIdentitySerializable
+import app.meetacy.backend.types.serialization.invitation.InvitationIdSerializable
+import app.meetacy.backend.types.serialization.user.UserIdSerializable
 import io.ktor.server.application.*
 import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GetInvitationParams(
-    val id: String,
+    val token: AccessIdentitySerializable,
+    val invitorUserIds: List<UserIdSerializable>?,
+    val invitationIds: List<InvitationIdSerializable>?
 )
 
 fun Route.readInvitationRouting(readInvitationRepository: ReadInvitationRepository) {
     get("/read") {
         val invitationParams: GetInvitationParams = call.receive()
 
-        val response = readInvitationRepository.getInvitation(invitationParams)
-
-        val httpStatusCode = when (response) {
+        when (val response = readInvitationRepository.getInvitation(invitationParams)) {
             is InvitationsGetResponse.Success -> {
-                HttpStatusCode.OK
+                call.respondSuccess(response.response)
             }
-
-            InvitationsGetResponse.NoPermissions -> {
-                HttpStatusCode.MethodNotAllowed
+            InvitationsGetResponse.InvalidInvitationIds -> {
+                call.respondFailure(Failure.InvalidInvitationIds)
             }
-
-            InvitationsGetResponse.NotFound -> {
-                HttpStatusCode.NotFound
+            InvitationsGetResponse.InvalidUserIds -> {
+                call.respondFailure(Failure.InvalidUserIds)
+            }
+            InvitationsGetResponse.OnlyUserIdsOrInvitationIdsAreAllowed -> {
+                call.respondFailure(Failure.OnlyUserIdsOrInvitationIdsAreAllowed)
+            }
+            InvitationsGetResponse.SpecifyAtLeastOneParam -> {
+                call.respondFailure(Failure.NullEditParams)
+            }
+            InvitationsGetResponse.Unauthorized -> {
+                call.respondFailure(Failure.InvalidToken)
             }
         }
-
-        call.respond(httpStatusCode, if (response is InvitationsGetResponse.Success) response else "")
     }
 }
 
 interface ReadInvitationRepository {
-    fun getInvitation(getInvitationParams: GetInvitationParams): InvitationsGetResponse
+    suspend fun getInvitation(getInvitationParams: GetInvitationParams): InvitationsGetResponse
 }
 
 sealed interface InvitationsGetResponse {
-    data class Success(val response: Invitation): InvitationsGetResponse
-    object NoPermissions: InvitationsGetResponse
-    object NotFound: InvitationsGetResponse
+    data class Success(val response: List<Invitation>): InvitationsGetResponse
+    object SpecifyAtLeastOneParam: InvitationsGetResponse
+    object OnlyUserIdsOrInvitationIdsAreAllowed: InvitationsGetResponse
+    object InvalidUserIds: InvitationsGetResponse
+    object InvalidInvitationIds: InvitationsGetResponse
+    object Unauthorized: InvitationsGetResponse
 }
