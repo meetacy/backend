7180cb84a1d7235d5e7dfdf1ea5ef3a704b29747
bpavuk <boopentiy@gmail.com>
2023-05-06 16:15:53 +0300

    WIP(#61): Added Invitation type

---
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/types/Invitation.kt
@@ -0,0 +1,18 @@
+package app.meetacy.backend.usecase.types
+
+import app.meetacy.backend.types.datetime.Date
+import app.meetacy.backend.types.invitation.InvitationIdentity
+import app.meetacy.backend.types.meeting.MeetingId
+import app.meetacy.backend.types.user.UserId
+
+data class Invitation(
+    val identity: InvitationIdentity,
+    val expiryDate: Date,
+    val invitedUserId: UserId,
+    val invitorUserId: UserId,
+    val meeting: MeetingId,
+    val title: String,
+    val description: String
+) {
+    val id = identity.id
+}
