d3c2bf797945dddd2b9a77e02059dd7df0b7ee07
bpavuk <boopentiy@gmail.com>
2023-05-08 21:51:46 +0300

    WIP(#61): Implemented database-usecase adapter

--- usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/accept/AcceptInvitationUsecase.kt
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/accept/AcceptInvitationUsecase.kt
@@ -1,6 +1,10 @@
 package app.meetacy.backend.usecase.invitations.accept
 
+import app.meetacy.backend.types.access.AccessIdentity
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
 import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.usecase.types.authorizeWithUserId
 
 class AcceptInvitationUsecase(
     private val authRepository: AuthRepository,
@@ -12,9 +16,20 @@ class AcceptInvitationUsecase(
         object Unauthorized: Result
     }
 
-    // TODO
+    suspend fun AccessIdentity.isInvited(invitationId: InvitationId): Result {
+        val userId = authRepository.authorizeWithUserId(this) { return Result.Unauthorized }
+
+        with(storage) {
+            if (userId.isInvited(invitationId)) {
+                userId.addToMeetingByInvitation(invitationId)
+            } else return Result.NotFound
+        }
+
+        return Result.Success
+    }
 
     interface Storage {
-
+        suspend fun UserId.isInvited(invitationId: InvitationId): Boolean
+        suspend fun UserId.addToMeetingByInvitation(invitationId: InvitationId)
     }
 }
