f5e26df36f40f9532ee0d8fbd9ab08dc95959408
bpavuk <boopentiy@gmail.com>
2023-05-09 00:11:07 +0300

    feat(#61): Implemented invitation acceptation usecase storage

--- database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/accept/AcceptInvitationUsecase.kt
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/accept/AcceptInvitationUsecase.kt
@@ -1,8 +1,41 @@
 package app.meetacy.backend.database.integration.invitation.accept
 
-import app.meetacy.backend.usecase.invitations.accept.AcceptInvitationUsecase
+import app.meetacy.backend.database.invitations.InvitationsTable
+import app.meetacy.backend.database.meetings.ParticipantsTable
+import app.meetacy.backend.types.datetime.Date
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.invitations.accept.AcceptInvitationUsecase.Storage
 import org.jetbrains.exposed.sql.Database
 
-class DatabaseAcceptInvitationStorage(db: Database): AcceptInvitationUsecase.Storage {
-    // TODO
+class DatabaseAcceptInvitationStorage(db: Database): Storage {
+    private val invitationsTable = InvitationsTable(db)
+    private val participantsTable = ParticipantsTable(db)
+
+    override suspend fun UserId.isInvited(invitationId: InvitationId): Storage.DatabaseResult {
+        with(invitationsTable) {
+            getInvitationsByInvitationIds(
+                this@isInvited,
+                listOf(invitationId)
+            ).firstOrNull() ?: return Storage.DatabaseResult.NotInvited
+
+            /* else */ return Storage.DatabaseResult.Success
+        }
+    }
+
+    override suspend fun UserId.addToMeetingByInvitation(invitationId: InvitationId): Storage.DatabaseResult {
+        val invitation = invitationsTable.getInvitationsByInvitationIds(
+            this,
+            listOf(invitationId)
+        ).firstOrNull() ?: return Storage.DatabaseResult.NotInvited
+        if (invitation.expiryDate < Date.today() || invitation.isAccepted == true) return Storage.DatabaseResult.InvitationExpired
+        with(participantsTable) {
+            addParticipant(
+                participantId = this@addToMeetingByInvitation,
+                meetingId = invitation.meeting
+            )
+        }
+        invitationsTable.markAsAccepted(this, invitationId)
+        return Storage.DatabaseResult.Success
+    }
 }
