4f3cfc5a88b9774f68d7bef4e2b32c070e135a72
bpavuk <boopentiy@gmail.com>
2023-05-10 12:34:30 +0300

    WIP(#61): Added adapter between the database and usecase

---
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/update/UpdateInvitationUsecase.kt
@@ -0,0 +1,70 @@
+package app.meetacy.backend.database.integration.invitation.update
+
+import app.meetacy.backend.database.invitations.InvitationsTable
+import app.meetacy.backend.database.meetings.MeetingsTable
+import app.meetacy.backend.database.meetings.ParticipantsTable
+import app.meetacy.backend.database.types.DatabaseInvitation
+import app.meetacy.backend.types.datetime.Date
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.meeting.MeetingId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.invitations.update.UpdateInvitationUsecase
+import org.jetbrains.exposed.sql.Database
+
+class DatabaseUpdateInvitationUsecase(db: Database): UpdateInvitationUsecase.Storage {
+    private val invitationsTable = InvitationsTable(db)
+    private val meetingsTable = MeetingsTable(db)
+    private val participantsTable = ParticipantsTable(db)
+
+    override suspend fun InvitationId.doesExist(): Boolean =
+        invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .singleOrNull() is DatabaseInvitation
+
+    override suspend fun MeetingId.doesExist(): Boolean =
+        meetingsTable
+            .getMeetingOrNull(this) != null
+
+    override suspend fun InvitationId.getInvitedUser(): UserId =
+        invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .single()
+            .invitedUserId
+
+    private suspend fun InvitationId.getInvitor(): UserId =
+        invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .single()
+            .invitorUserId
+
+    override suspend fun MeetingId.ableToInvite(invitorId: UserId, invitedId: UserId): Boolean {
+        val invitorIsParticipating = participantsTable
+            .isParticipating(this, invitorId)
+
+        val invitedIsParticipating = participantsTable
+            .isParticipating(this, invitedId)
+
+        return invitorIsParticipating && !invitedIsParticipating
+        // TODO: Due to limited possibilities of MeetingsTable validation ends up with this
+    }
+
+    override suspend fun InvitationId.isCreatedBy(userId: UserId): Boolean =
+        invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .single()
+            .invitorUserId == userId
+
+    override suspend fun InvitationId.update(
+        title: String?,
+        description: String?,
+        expiryDate: Date?,
+        meetingId: MeetingId?
+    ): Boolean =
+        invitationsTable
+            .update(
+                invitationId = this,
+                invitorUserId = getInvitor(),
+                title, description, expiryDate, meetingId
+            )
+
+}
