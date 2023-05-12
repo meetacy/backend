3c2ed1f9f725eb393b00289d040b3fec333af392
bpavuk <boopentiy@gmail.com>
2023-05-12 17:18:50 +0300

    WIP(#61): Reworked invitation acceptation logic, ditched DatabaseResult sealed interface

--- usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/accept/AcceptInvitationUsecase.kt
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/accept/AcceptInvitationUsecase.kt
@@ -15,32 +15,28 @@ class AcceptInvitationUsecase(
         object NotFound: Result
         object Unauthorized: Result
         object InvitationExpired: Result
+        object MeetingNotFound: Result
     }
 
     suspend fun AccessIdentity.addToMeetingByInvitation(invitationId: InvitationId): Result {
         val userId = authRepository.authorizeWithUserId(this) { return Result.Unauthorized }
 
         with(storage) {
-            if (userId.isInvited(invitationId) == Storage.DatabaseResult.Success) {
-                return userId.addToMeetingByInvitation(invitationId).toUsecase()
-            } else return Result.NotFound
+            when {
+                !userId.isInvited(invitationId) -> return Result.NotFound
+                invitationId.isExpired() -> return Result.InvitationExpired
+                !invitationId.doesMeetingExist() -> return Result.MeetingNotFound
+                else -> {
+                    return if (userId.addToMeetingByInvitation(invitationId)) Result.Success else Result.NotFound
+                }
+            }
         }
     }
 
     interface Storage {
-        sealed interface DatabaseResult {
-            object Success: DatabaseResult
-            object InvitationExpired: DatabaseResult
-            object NotInvited: DatabaseResult
-        }
-
-        suspend fun UserId.isInvited(invitationId: InvitationId): DatabaseResult
-        suspend fun UserId.addToMeetingByInvitation(invitationId: InvitationId): DatabaseResult
-    }
-
-    private fun Storage.DatabaseResult.toUsecase(): Result = when (this) {
-        Storage.DatabaseResult.InvitationExpired -> Result.InvitationExpired
-        Storage.DatabaseResult.NotInvited -> Result.NotFound
-        Storage.DatabaseResult.Success -> Result.Success
+        suspend fun UserId.isInvited(invitationId: InvitationId): Boolean
+        suspend fun UserId.addToMeetingByInvitation(invitationId: InvitationId): Boolean
+        suspend fun InvitationId.isExpired(): Boolean
+        suspend fun InvitationId.doesMeetingExist(): Boolean
     }
 }
