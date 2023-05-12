75e4288092254753cfd3d2d935c13acb73650562
bpavuk <boopentiy@gmail.com>
2023-05-12 18:13:24 +0300

    WIP(#61): Updated adapter

--- database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/read/ReadInvitationUsecase.kt
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/read/ReadInvitationUsecase.kt
@@ -2,46 +2,48 @@ package app.meetacy.backend.database.integration.invitation.read
 
 import app.meetacy.backend.database.invitations.InvitationsTable
 import app.meetacy.backend.database.types.DatabaseInvitation
+import app.meetacy.backend.database.users.UsersTable
 import app.meetacy.backend.types.invitation.InvitationId
 import app.meetacy.backend.types.user.UserId
 import app.meetacy.backend.usecase.invitations.read.ReadInvitationUsecase
-import app.meetacy.backend.usecase.invitations.read.ReadInvitationUsecase.Storage.DatabaseResult
 import app.meetacy.backend.usecase.types.Invitation
 import org.jetbrains.exposed.sql.Database
 
 
 class DatabaseReadInvitationStorage(db: Database): ReadInvitationUsecase.Storage {
     private val invitationsTable = InvitationsTable(db)
+    private val usersTable = UsersTable(db)
 
-    override suspend fun UserId.getInvitations(): DatabaseResult {
+    override suspend fun UserId.getInvitations(): List<Invitation> {
         val invitations = invitationsTable.getInvitations(invitedUserId = this)
-        return DatabaseResult.Success(invitations.map { it.toInvitation() })
+        return invitations.map { it.toInvitation() }
     }
 
-    override suspend fun UserId.getInvitations(from: List<UserId>): DatabaseResult {
+    override suspend fun UserId.getInvitations(from: List<UserId>): List<Invitation> {
         val invitations = invitationsTable
             .getInvitations(invitedUserId = this, invitorUserIdsList = from)
             .filter { it.invitorUserId in from }
 
-        val retrievedFrom = invitations.map { it.invitorUserId }.toSet()
-
-        return if (retrievedFrom != from.toSet()) {
-            DatabaseResult.UsersNotFound
-        } else {
-            DatabaseResult.Success(invitations.map { it.toInvitation() })
-        }
+        return invitations.map { it.toInvitation() }
     }
 
-    override suspend fun UserId.getInvitationsByIds(ids: List<InvitationId>): DatabaseResult {
+    override suspend fun UserId.getInvitationsByIds(ids: List<InvitationId>): List<Invitation> {
         val invitations = invitationsTable
             .getInvitationsByInvitationIds(this, ids)
 
-        return if (invitations.map { it.id }.toSet() != ids) {
-            DatabaseResult.InvitationsNotFound
-        } else {
-            DatabaseResult.Success(invitations.map { it.toInvitation() })
-        }
+        return invitations.map { it.toInvitation() }
     }
+
+    override suspend fun UserId.doesExist(): Boolean =
+        usersTable
+            .getUsersOrNull(listOf(this))
+            .singleOrNull() != null
+
+    override suspend fun InvitationId.doesExist(): Boolean =
+        invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .singleOrNull() != null
+
 
     private fun DatabaseInvitation.toInvitation() = Invitation(
         identity,
