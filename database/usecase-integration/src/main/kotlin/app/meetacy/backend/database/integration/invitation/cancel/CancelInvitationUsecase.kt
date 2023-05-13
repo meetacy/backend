8fd8ee68333baac19b44b27093964c78a7e5820a
bpavuk <boopentiy@gmail.com>
2023-05-13 21:09:00 +0300

    WIP(#61): added adapter between usecase and database

---
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/invitation/cancel/CancelInvitationUsecase.kt
@@ -0,0 +1,41 @@
+package app.meetacy.backend.database.integration.invitation.cancel
+
+import app.meetacy.backend.database.invitations.InvitationsTable
+import app.meetacy.backend.database.users.UsersTable
+import app.meetacy.backend.types.datetime.Date
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.invitations.cancel.CancelInvitationUsecase
+import org.jetbrains.exposed.sql.Database
+
+class DatabaseCancelInvitationStorage(db: Database): CancelInvitationUsecase.Storage {
+    private val invitationsTable = InvitationsTable(db)
+    private val usersTable = UsersTable(db)
+
+    override suspend fun UserId.isInvitor(invitationId: InvitationId): Boolean =
+        invitationsTable.getInvitationsByInvitationIds(listOf(invitationId)).singleOrNull()!!.invitedUserId == this
+
+    override suspend fun InvitationId.doesExist(): Boolean {
+        val invitation = invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .singleOrNull()
+
+        return invitation != null && invitation.isAccepted == null
+    }
+
+    override suspend fun InvitationId.isExpired(): Boolean {
+        val invitation = invitationsTable
+            .getInvitationsByInvitationIds(listOf(this))
+            .singleOrNull()
+
+        return invitation!!.expiryDate > Date.today()
+    }
+
+    override suspend fun UserId.doesExist(): Boolean {
+        return usersTable.getUsersOrNull(listOf(this)).singleOrNull() != null
+    }
+
+    override suspend fun InvitationId.cancel(): Boolean =
+        invitationsTable.cancel(this)
+
+}
