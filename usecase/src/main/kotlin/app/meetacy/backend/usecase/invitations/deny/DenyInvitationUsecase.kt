5890e63c370568f22aca81d0a255766831795edc
bpavuk <boopentiy@gmail.com>
2023-05-13 11:42:34 +0300

    WIP(#61): Added usecase for invitation denying

---
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/deny/DenyInvitationUsecase.kt
@@ -0,0 +1,36 @@
+package app.meetacy.backend.usecase.invitations.deny
+
+import app.meetacy.backend.types.access.AccessIdentity
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.usecase.types.authorizeWithUserId
+
+class DenyInvitationUsecase(
+    private val authRepository: AuthRepository,
+    private val storage: Storage
+) {
+    sealed interface Result {
+        object Success: Result
+        object Unauthorized: Result
+        object NoPermissions: Result
+        object NotFound: Result
+    }
+
+    suspend fun InvitationId.markAsDenied(token: AccessIdentity): Result {
+        val userId = authRepository.authorizeWithUserId(token) { return Result.Unauthorized }
+
+        with(storage) {
+            if (!doesExist()) return Result.NotFound
+            if (!userId.isInvited(this@markAsDenied)) return Result.NoPermissions  // yes, I love smart this
+            markAsDenied()
+        }
+        return Result.Success
+    }
+
+    interface Storage {
+        suspend fun UserId.isInvited(invitation: InvitationId): Boolean
+        suspend fun InvitationId.doesExist(): Boolean
+        suspend fun InvitationId.markAsDenied()
+    }
+}
