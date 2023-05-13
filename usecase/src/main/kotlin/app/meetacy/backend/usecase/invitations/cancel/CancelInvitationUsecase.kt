d1129c6964917af278a12d2bbee75bd21edfcd3d
bpavuk <boopentiy@gmail.com>
2023-05-13 20:45:05 +0300

    WIP(#61): added usecase for invitation cancellation

---
+++ usecase/src/main/kotlin/app/meetacy/backend/usecase/invitations/cancel/CancelInvitationUsecase.kt
@@ -0,0 +1,40 @@
+package app.meetacy.backend.usecase.invitations.cancel
+
+import app.meetacy.backend.types.access.AccessIdentity
+import app.meetacy.backend.types.invitation.InvitationId
+import app.meetacy.backend.types.user.UserId
+import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.usecase.types.authorizeWithUserId
+
+class CancelInvitationUsecase(
+    val authRepository: AuthRepository,
+    val storage: Storage
+) {
+    sealed interface Result {
+        object Success: Result
+        object Unauthorized: Result
+        object NoPermissions: Result
+        object NotFound: Result
+        object UserNotFound: Result
+    }
+
+    suspend fun InvitationId.cancel(token: AccessIdentity): Result {
+        val userId = authRepository.authorizeWithUserId(token) { return Result.Unauthorized }
+
+        with(storage) {
+            if (!doesExist()) return Result.NotFound
+            if (!userId.doesExist()) return Result.UserNotFound
+            if (!userId.isInvitor(this@cancel)) return Result.NoPermissions
+            cancel()
+        }
+
+        return Result.Success
+    }
+
+    interface Storage {
+        fun UserId.isInvitor(invitationId: InvitationId): Boolean
+        fun InvitationId.doesExist(): Boolean
+        fun UserId.doesExist(): Boolean
+        fun InvitationId.cancel(): Boolean
+    }
+}
