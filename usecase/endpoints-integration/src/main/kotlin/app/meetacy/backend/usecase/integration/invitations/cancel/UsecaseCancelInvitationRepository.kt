cb9ba9e34aee2d398d5a217868fac150faec39f8
bpavuk <boopentiy@gmail.com>
2023-05-14 18:34:50 +0300

    WIP(#61): added adapter between endpoint and usecase

---
+++ usecase/endpoints-integration/src/main/kotlin/app/meetacy/backend/usecase/integration/invitations/cancel/UsecaseCancelInvitationRepository.kt
@@ -0,0 +1,23 @@
+package app.meetacy.backend.usecase.integration.invitations.cancel
+
+import app.meetacy.backend.endpoint.invitations.cancel.CancelInvitationForm
+import app.meetacy.backend.endpoint.invitations.cancel.CancelInvitationRepository
+import app.meetacy.backend.endpoint.invitations.cancel.CancelInvitationResponse
+import app.meetacy.backend.usecase.invitations.cancel.CancelInvitationUsecase
+
+class UsecaseCancelInvitationRepository(
+    private val usecase: CancelInvitationUsecase
+): CancelInvitationRepository {
+
+    override suspend fun CancelInvitationForm.cancel(): CancelInvitationResponse = with(usecase) {
+        invitationId.type().cancel(token.type()).toEndpoint()
+    }
+
+    private fun CancelInvitationUsecase.Result.toEndpoint(): CancelInvitationResponse = when (this) {
+        CancelInvitationUsecase.Result.NoPermissions -> CancelInvitationResponse.NoPermissions
+        CancelInvitationUsecase.Result.NotFound -> CancelInvitationResponse.NotFound
+        CancelInvitationUsecase.Result.Success -> CancelInvitationResponse.Success
+        CancelInvitationUsecase.Result.Unauthorized -> CancelInvitationResponse.Unauthorized
+        CancelInvitationUsecase.Result.UserNotFound -> CancelInvitationResponse.UserNotFound
+    }
+}
