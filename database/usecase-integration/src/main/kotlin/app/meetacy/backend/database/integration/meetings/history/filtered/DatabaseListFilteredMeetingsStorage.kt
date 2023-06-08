1393d1791ad8490f65c4b0fbaef088390d71df2c
bpavuk <boopentiy@gmail.com>
2023-06-08 20:59:18 +0300

    WIP(#61): refactored getFilteredMeetings function according to @y9san9's guidelines

--- database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/meetings/history/filtered/DatabaseListFilteredMeetingsStorage.kt
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/meetings/history/filtered/DatabaseListFilteredMeetingsStorage.kt
@@ -6,6 +6,10 @@ import app.meetacy.backend.types.meeting.MeetingId
 import app.meetacy.backend.types.paging.PagingId
 import app.meetacy.backend.types.paging.PagingResult
 import app.meetacy.backend.types.user.UserId
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
 import org.jetbrains.exposed.sql.Database
 
 class DatabaseListFilteredMeetingsStorage(db: Database) {
@@ -16,14 +20,12 @@ class DatabaseListFilteredMeetingsStorage(db: Database) {
         pagingId: PagingId?,
         filter: suspend (MeetingId) -> Boolean
     ): PagingResult<List<MeetingId>> {
-        val result = mutableListOf<MeetingId>()
-        var currentPagingId = pagingId
-        while (result.size < amount.int) {
-            val paging = participantsTable.getJoinHistory(memberId, amount, currentPagingId)
-            result.addAll(paging.data.filter { filter(it) })
-            currentPagingId = paging.nextPagingId
-            if (currentPagingId == null) break
-        }
-        return PagingResult(data = result.take(amount.int), nextPagingId = currentPagingId)
+        val data = participantsTable.getJoinHistoryFlow(memberId, pagingId)
+            .map {
+                it.data to it.nextPagingId
+            }.filter { filter(it.first) }.take(amount.int).toList()
+        val nextPagingId = data.last().second
+
+        return PagingResult(data.map { (meetingId) -> meetingId }, nextPagingId)
     }
 }
