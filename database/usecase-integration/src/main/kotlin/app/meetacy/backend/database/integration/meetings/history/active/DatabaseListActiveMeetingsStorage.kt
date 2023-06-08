f498f06cdf4e252e8e6d0842901ce563535c0189
bpavuk <boopentiy@gmail.com>
2023-06-08 15:47:34 +0300

    WIP(#61): refactored active meetings endpoint

--- database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/meetings/history/active/DatabaseListActiveMeetingsStorage.kt
+++ database/usecase-integration/src/main/kotlin/app/meetacy/backend/database/integration/meetings/history/active/DatabaseListActiveMeetingsStorage.kt
@@ -1,7 +1,7 @@
 package app.meetacy.backend.database.integration.meetings.history.active
 
+import app.meetacy.backend.database.integration.meetings.history.filtered.DatabaseListFilteredMeetingsStorage
 import app.meetacy.backend.database.meetings.MeetingsTable
-import app.meetacy.backend.database.meetings.ParticipantsTable
 import app.meetacy.backend.types.amount.Amount
 import app.meetacy.backend.types.datetime.Date
 import app.meetacy.backend.types.meeting.MeetingId
@@ -11,22 +11,17 @@ import app.meetacy.backend.types.user.UserId
 import app.meetacy.backend.usecase.meetings.history.active.ListMeetingsActiveUsecase
 import org.jetbrains.exposed.sql.Database
 
-class DatabaseListActiveMeetingsStorage(db: Database): ListMeetingsActiveUsecase.Storage {
-    private val participantsTable = ParticipantsTable(db)
+class DatabaseListActiveMeetingsStorage(
+    private val listFilteredMeetingsStorage: DatabaseListFilteredMeetingsStorage,
+    db: Database
+) : ListMeetingsActiveUsecase.Storage {
     private val meetingsTable = MeetingsTable(db)
+    private val filter: suspend (MeetingId) -> Boolean = { meetingsTable.getMeeting(it).date >= Date.today() }
+
     override suspend fun getActiveMeetings(
         memberId: UserId,
         amount: Amount,
         pagingId: PagingId?
-    ): PagingResult<List<MeetingId>> {
-        val result = mutableListOf<MeetingId>()
-        var currentPagingId = pagingId
-        while (result.size < amount.int) {
-            val paging = participantsTable.getJoinHistory(memberId, amount, currentPagingId)
-            result.addAll(paging.data.filter { meetingsTable.getMeeting(it).date >= Date.today() })
-            currentPagingId = paging.nextPagingId
-            if (currentPagingId == null) break
-        }
-        return PagingResult(data = result.take(amount.int), nextPagingId = currentPagingId)
-    }
+    ): PagingResult<List<MeetingId>> = listFilteredMeetingsStorage
+        .getFilteredMeetings(memberId, amount, pagingId, filter)
 }
