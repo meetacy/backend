177365f153efc561233f4ca7f8f5a38218d465ef
bpavuk <boopentiy@gmail.com>
2023-06-08 15:08:40 +0300

    WIP(#61): linked endpoint

--- endpoints/src/main/kotlin/app/meetacy/backend/endpoint/meetings/history/Routing.kt
+++ endpoints/src/main/kotlin/app/meetacy/backend/endpoint/meetings/history/Routing.kt
@@ -4,14 +4,18 @@ import app.meetacy.backend.endpoint.meetings.history.active.ListMeetingsActiveRe
 import app.meetacy.backend.endpoint.meetings.history.active.meetingsHistoryActive
 import app.meetacy.backend.endpoint.meetings.history.list.ListMeetingsHistoryRepository
 import app.meetacy.backend.endpoint.meetings.history.list.listMeetingsHistory
+import app.meetacy.backend.endpoint.meetings.history.past.ListMeetingsPastRepository
+import app.meetacy.backend.endpoint.meetings.history.past.meetingsHistoryPast
 import io.ktor.server.routing.*
 
 class MeetingsHistoryDependencies(
     val listMeetingsHistoryRepository: ListMeetingsHistoryRepository,
-    val meetingsActiveRepository: ListMeetingsActiveRepository
+    val meetingsActiveRepository: ListMeetingsActiveRepository,
+    val meetingsPastRepository: ListMeetingsPastRepository
 )
 
 fun Route.meetingsHistory(dependencies: MeetingsHistoryDependencies) = route("/history") {
     listMeetingsHistory(dependencies.listMeetingsHistoryRepository)
     meetingsHistoryActive(dependencies.meetingsActiveRepository)
+    meetingsHistoryPast(dependencies.meetingsPastRepository)
 }
