c9d4f688a656e5de6588e783272ef1406ab6ab86
bpavuk <boopentiy@gmail.com>
2023-06-28 15:58:14 +0300

    WIP: moved invitation dependencies to separate factory

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/Factory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/Factory.kt
@@ -1,29 +1,15 @@
 package app.meetacy.backend.infrastructure
 
-import app.meetacy.backend.database.integration.invitations.accept.DatabaseAcceptInvitationStorage
-import app.meetacy.backend.database.integration.invitations.cancel.DatabaseCancelInvitationStorage
-import app.meetacy.backend.database.integration.invitations.create.DatabaseCreateInvitationStorage
-import app.meetacy.backend.database.integration.invitations.deny.DatabaseDenyInvitationStorage
 import app.meetacy.backend.database.integration.meetings.DatabaseCheckMeetingsViewRepository
 import app.meetacy.backend.database.integration.meetings.get.DatabaseGetMeetingsViewsViewMeetingsRepository
 import app.meetacy.backend.database.integration.notifications.AddNotificationUsecase
 import app.meetacy.backend.database.integration.types.*
 import app.meetacy.backend.database.integration.updates.stream.StreamUpdatesUsecase
 import app.meetacy.backend.database.integration.updates.stream.UpdatesMiddleware
-import app.meetacy.backend.endpoint.invitations.InvitationsDependencies
 import app.meetacy.backend.endpoint.startEndpoints
 import app.meetacy.backend.endpoint.updates.UpdatesDependencies
-import app.meetacy.backend.hash.integration.DefaultHashGenerator
 import app.meetacy.backend.infrastructure.factories.*
-import app.meetacy.backend.usecase.integration.invitations.accept.UsecaseAcceptInvitationRepository
-import app.meetacy.backend.usecase.integration.invitations.cancel.UsecaseCancelInvitationRepository
-import app.meetacy.backend.usecase.integration.invitations.create.UsecaseCreateInvitationRepository
-import app.meetacy.backend.usecase.integration.invitations.deny.UsecaseDenyInvitationRepository
 import app.meetacy.backend.usecase.integration.updates.stream.UsecaseStreamUpdatesRepository
-import app.meetacy.backend.usecase.invitations.accept.AcceptInvitationUsecase
-import app.meetacy.backend.usecase.invitations.cancel.CancelInvitationUsecase
-import app.meetacy.backend.usecase.invitations.create.CreateInvitationUsecase
-import app.meetacy.backend.usecase.invitations.deny.DenyInvitationUsecase
 import org.jetbrains.exposed.sql.Database
 
 fun startEndpoints(
@@ -67,39 +53,11 @@ fun startEndpoints(
             getMeetingsViewsRepository, getUsersViewsRepository, viewMeetingsRepository
         ),
         notificationsDependencies = notificationDependenciesFactory(
-            db, authRepository,
-            getMeetingsViewsRepository,
+            db, authRepository, getMeetingsViewsRepository,
             getUsersViewsRepository
         ),
         filesDependencies = fileDependenciesFactory(db, authRepository, filesBasePath, filesLimit),
-        invitationsDependencies = InvitationsDependencies(
-            invitationsCreateRepository = UsecaseCreateInvitationRepository(
-                usecase = CreateInvitationUsecase(
-                    authRepository = authRepository,
-                    storage = DatabaseCreateInvitationStorage(db, addNotificationUsecase),
-                    hashGenerator = DefaultHashGenerator,
-                    invitationsRepository = getInvitationsViewsRepository
-                )
-            ),
-            invitationsAcceptRepository = UsecaseAcceptInvitationRepository(
-                usecase = AcceptInvitationUsecase(
-                    authRepository = authRepository,
-                    storage = DatabaseAcceptInvitationStorage(db)
-                )
-            ),
-            invitationsDenyRepository = UsecaseDenyInvitationRepository(
-                usecase = DenyInvitationUsecase(
-                    authRepository = authRepository,
-                    storage = DatabaseDenyInvitationStorage(db)
-                )
-            ),
-            invitationCancelRepository = UsecaseCancelInvitationRepository(
-                usecase = CancelInvitationUsecase(
-                    authRepository = authRepository,
-                    storage = DatabaseCancelInvitationStorage(db)
-                )
-            )
-        ),
+        invitationsDependencies = invitationDependenciesFactory(db, addNotificationUsecase, authRepository, getInvitationsViewsRepository),
         updatesDependencies = UpdatesDependencies(
             streamUpdatesRepository = UsecaseStreamUpdatesRepository(
                 usecase = StreamUpdatesUsecase(
