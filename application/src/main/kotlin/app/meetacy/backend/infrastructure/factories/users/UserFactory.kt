4550920da4f30aeed14097b9ee9178f18412c674
bpavuk <boopentiy@gmail.com>
2023-07-01 13:03:07 +0300

    WIP: beaten up the user editing dependencies

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/users/UserFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/users/UserFactory.kt
@@ -1,23 +1,17 @@
 package app.meetacy.backend.infrastructure.factories.users
 
-import app.meetacy.backend.database.integration.users.edit.DatabaseEditUserStorage
 import app.meetacy.backend.endpoint.users.UsersDependencies
 import app.meetacy.backend.infrastructure.factories.auth.authRepository
-import app.meetacy.backend.infrastructure.factories.files.filesRepository
-import app.meetacy.backend.usecase.integration.users.edit.UsecaseEditUserRepository
+import app.meetacy.backend.infrastructure.factories.users.edit.editUserRepository
 import app.meetacy.backend.usecase.integration.users.get.UsecaseUserRepository
 import app.meetacy.backend.usecase.types.AuthRepository
-import app.meetacy.backend.usecase.types.FilesRepository
 import app.meetacy.backend.usecase.types.GetUsersViewsRepository
-import app.meetacy.backend.usecase.users.edit.EditUserUsecase
 import app.meetacy.backend.usecase.users.get.GetUserSafeUsecase
-import app.meetacy.backend.utf8.integration.DefaultUtf8Checker
 import org.jetbrains.exposed.sql.Database
 
 fun userDependenciesFactory(
     db: Database,
     authRepository: AuthRepository = authRepository(db),
-    filesRepository: FilesRepository = filesRepository(db),
     getUsersViewsRepository: GetUsersViewsRepository = getUserViewsRepository(db)
 ): UsersDependencies = UsersDependencies(
     getUserRepository = UsecaseUserRepository(
@@ -26,12 +20,5 @@ fun userDependenciesFactory(
             usersViewsRepository = getUsersViewsRepository
         )
     ),
-    editUserRepository = UsecaseEditUserRepository(
-        usecase = EditUserUsecase(
-            storage = DatabaseEditUserStorage(db),
-            authRepository = authRepository,
-            filesRepository = filesRepository,
-            utf8Checker = DefaultUtf8Checker
-        )
-    )
+    editUserRepository = editUserRepository(db)
 )
