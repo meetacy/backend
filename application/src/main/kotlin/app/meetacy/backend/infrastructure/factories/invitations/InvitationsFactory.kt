1337802748ad62b2e09292d794931fa83f57e8d3
bpavuk <boopentiy@gmail.com>
2023-07-01 12:47:27 +0300

    WIP: beaten up the invitation denying dependencies

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/invitations/InvitationsFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/invitations/InvitationsFactory.kt
@@ -1,27 +1,17 @@
 package app.meetacy.backend.infrastructure.factories.invitations
 
-import app.meetacy.backend.database.integration.invitations.deny.DatabaseDenyInvitationStorage
 import app.meetacy.backend.endpoint.invitations.InvitationsDependencies
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
 import app.meetacy.backend.infrastructure.factories.invitations.accept.acceptInvitationRepository
 import app.meetacy.backend.infrastructure.factories.invitations.cancel.cancelInvitationRepository
 import app.meetacy.backend.infrastructure.factories.invitations.create.createInvitationRepository
-import app.meetacy.backend.usecase.integration.invitations.deny.UsecaseDenyInvitationRepository
-import app.meetacy.backend.usecase.invitations.deny.DenyInvitationUsecase
-import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.infrastructure.factories.invitations.deny.denyInvitationRepository
 import org.jetbrains.exposed.sql.Database
 
 fun invitationDependenciesFactory(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db)
+    db: Database
 ): InvitationsDependencies = InvitationsDependencies(
     invitationsCreateRepository = createInvitationRepository(db),
     invitationsAcceptRepository = acceptInvitationRepository(db),
-    invitationsDenyRepository = UsecaseDenyInvitationRepository(
-        usecase = DenyInvitationUsecase(
-            authRepository = authRepository,
-            storage = DatabaseDenyInvitationStorage(db)
-        )
-    ),
+    invitationsDenyRepository = denyInvitationRepository(db),
     invitationCancelRepository = cancelInvitationRepository(db)
 )
