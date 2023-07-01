7426bf7db981d20e453a445c80e0025b094d646b
bpavuk <boopentiy@gmail.com>
2023-07-01 11:29:02 +0300

    WIP: beaten-up confirm email repo

---
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/email/ConfirmEmailRepositoryBuilder.kt
@@ -0,0 +1,13 @@
+package app.meetacy.backend.infrastructure.factories.auth.email
+
+import app.meetacy.backend.database.integration.email.DatabaseConfirmEmailStorage
+import app.meetacy.backend.endpoint.auth.email.confirm.ConfirmEmailRepository
+import app.meetacy.backend.usecase.email.ConfirmEmailUsecase
+import app.meetacy.backend.usecase.integration.email.confirm.UsecaseConfirmEmailRepository
+import org.jetbrains.exposed.sql.Database
+
+fun confirmEmailRepository(db: Database): ConfirmEmailRepository = UsecaseConfirmEmailRepository(
+    usecase = ConfirmEmailUsecase(
+        storage = DatabaseConfirmEmailStorage(db)
+    )
+)
--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/email/EmailDependenciesBuilder.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/email/EmailDependenciesBuilder.kt
@@ -1,18 +1,11 @@
 package app.meetacy.backend.infrastructure.factories.auth.email
 
-import app.meetacy.backend.database.integration.email.DatabaseConfirmEmailStorage
 import app.meetacy.backend.endpoint.auth.email.EmailDependencies
-import app.meetacy.backend.usecase.email.ConfirmEmailUsecase
-import app.meetacy.backend.usecase.integration.email.confirm.UsecaseConfirmEmailRepository
 import org.jetbrains.exposed.sql.Database
 
 fun emailDependencies(
     db: Database
 ): EmailDependencies = EmailDependencies(
     linkEmailRepository = linkEmailRepository(db),
-    confirmEmailRepository = UsecaseConfirmEmailRepository(
-        usecase = ConfirmEmailUsecase(
-            storage = DatabaseConfirmEmailStorage(db)
-        )
-    )
+    confirmEmailRepository = confirmEmailRepository(db)
 )
