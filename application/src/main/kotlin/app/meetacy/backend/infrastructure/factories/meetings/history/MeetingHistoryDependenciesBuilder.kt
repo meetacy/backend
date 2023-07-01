2efc4c5f95051808a1b3ff31a842f8d443bf0228
bpavuk <boopentiy@gmail.com>
2023-07-01 11:57:50 +0300

    WIP: beaten up the list past meetings history repository

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/meetings/history/MeetingHistoryDependenciesBuilder.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/meetings/history/MeetingHistoryDependenciesBuilder.kt
@@ -1,27 +1,12 @@
 package app.meetacy.backend.infrastructure.factories.meetings.history
 
-import app.meetacy.backend.database.integration.meetings.history.past.DatabaseListPastMeetingsStorage
 import app.meetacy.backend.endpoint.meetings.history.MeetingsHistoryDependencies
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
-import app.meetacy.backend.infrastructure.factories.meetings.getMeetingsViewsRepository
-import app.meetacy.backend.usecase.integration.meetings.history.past.UsecaseListPastMeetingsRepository
-import app.meetacy.backend.usecase.meetings.history.past.ListMeetingsPastUsecase
-import app.meetacy.backend.usecase.types.AuthRepository
-import app.meetacy.backend.usecase.types.GetMeetingsViewsRepository
 import org.jetbrains.exposed.sql.Database
 
 fun meetingHistoryDependencies(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db),
-    getMeetingsViewsRepository: GetMeetingsViewsRepository = getMeetingsViewsRepository(db)
+    db: Database
 ): MeetingsHistoryDependencies = MeetingsHistoryDependencies(
     listMeetingsHistoryRepository = listMeetingsHistoryRepository(db),
     meetingsActiveRepository = listActiveMeetingsRepository(db),
-    meetingsPastRepository = UsecaseListPastMeetingsRepository(
-        usecase = ListMeetingsPastUsecase(
-            authRepository,
-            storage = DatabaseListPastMeetingsStorage(db),
-            getMeetingsViewsRepository = getMeetingsViewsRepository
-        )
-    ),
+    meetingsPastRepository = listPastMeetingsRepository(db),
 )
