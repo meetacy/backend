ff08642f7686f841a874a0f92f299ee188c2989c
bpavuk <boopentiy@gmail.com>
2023-07-01 11:27:38 +0300

    WIP: beaten-up link email repo

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/email/EmailDependenciesBuilder.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/email/EmailDependenciesBuilder.kt
@@ -1,30 +1,15 @@
 package app.meetacy.backend.infrastructure.factories.auth.email
 
 import app.meetacy.backend.database.integration.email.DatabaseConfirmEmailStorage
-import app.meetacy.backend.database.integration.email.DatabaseLinkEmailMailer
-import app.meetacy.backend.database.integration.email.DatabaseLinkEmailStorage
 import app.meetacy.backend.endpoint.auth.email.EmailDependencies
-import app.meetacy.backend.hash.integration.DefaultHashGenerator
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
 import app.meetacy.backend.usecase.email.ConfirmEmailUsecase
-import app.meetacy.backend.usecase.email.LinkEmailUsecase
 import app.meetacy.backend.usecase.integration.email.confirm.UsecaseConfirmEmailRepository
-import app.meetacy.backend.usecase.integration.email.link.UsecaseLinkEmailRepository
-import app.meetacy.backend.usecase.types.AuthRepository
 import org.jetbrains.exposed.sql.Database
 
 fun emailDependencies(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db)
+    db: Database
 ): EmailDependencies = EmailDependencies(
-    linkEmailRepository = UsecaseLinkEmailRepository(
-        usecase = LinkEmailUsecase(
-            storage = DatabaseLinkEmailStorage(db),
-            mailer = DatabaseLinkEmailMailer,
-            hashGenerator = DefaultHashGenerator,
-            authRepository = authRepository
-        )
-    ),
+    linkEmailRepository = linkEmailRepository(db),
     confirmEmailRepository = UsecaseConfirmEmailRepository(
         usecase = ConfirmEmailUsecase(
             storage = DatabaseConfirmEmailStorage(db)
