99b2ff5b1a05bdf114af853c20d30ecc2fc15c4d
bpavuk <boopentiy@gmail.com>
2023-07-01 12:56:41 +0300

    WIP: beaten up the notification reading dependencies

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/notifications/NotificationFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/notifications/NotificationFactory.kt
@@ -1,23 +1,13 @@
 package app.meetacy.backend.infrastructure.factories.notifications
 
-import app.meetacy.backend.database.integration.notifications.DatabaseReadNotificationsStorage
 import app.meetacy.backend.endpoint.notifications.NotificationsDependencies
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
 import app.meetacy.backend.infrastructure.factories.notifications.list.listNotificationRepository
-import app.meetacy.backend.usecase.integration.notifications.read.UsecaseReadNotificationsRepository
-import app.meetacy.backend.usecase.notifications.ReadNotificationsUsecase
-import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.infrastructure.factories.notifications.read.readNotificationRepository
 import org.jetbrains.exposed.sql.Database
 
 fun notificationDependenciesFactory(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db)
+    db: Database
 ): NotificationsDependencies = NotificationsDependencies(
     listNotificationsRepository = listNotificationRepository(db),
-    readNotificationsRepository = UsecaseReadNotificationsRepository(
-        usecase = ReadNotificationsUsecase(
-            authRepository = authRepository,
-            storage = DatabaseReadNotificationsStorage(db)
-        )
-    )
+    readNotificationsRepository = readNotificationRepository(db)
 )
