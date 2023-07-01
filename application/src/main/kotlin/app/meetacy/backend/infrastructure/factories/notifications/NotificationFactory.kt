ce2fb3e7222ea1780e978d7d024820e0b7dadcbc
bpavuk <boopentiy@gmail.com>
2023-07-01 12:54:51 +0300

    WIP: beaten up the notification listing dependencies

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/notifications/NotificationFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/notifications/NotificationFactory.kt
@@ -1,33 +1,19 @@
 package app.meetacy.backend.infrastructure.factories.notifications
 
 import app.meetacy.backend.database.integration.notifications.DatabaseReadNotificationsStorage
-import app.meetacy.backend.database.integration.notifications.GetNotificationsUsecase
 import app.meetacy.backend.endpoint.notifications.NotificationsDependencies
 import app.meetacy.backend.infrastructure.factories.auth.authRepository
-import app.meetacy.backend.infrastructure.factories.meetings.get.getMeetingsViewsRepository
-import app.meetacy.backend.infrastructure.factories.users.getUserViewsRepository
-import app.meetacy.backend.usecase.integration.notifications.get.UsecaseListNotificationsRepository
+import app.meetacy.backend.infrastructure.factories.notifications.list.listNotificationRepository
 import app.meetacy.backend.usecase.integration.notifications.read.UsecaseReadNotificationsRepository
 import app.meetacy.backend.usecase.notifications.ReadNotificationsUsecase
 import app.meetacy.backend.usecase.types.AuthRepository
-import app.meetacy.backend.usecase.types.GetMeetingsViewsRepository
-import app.meetacy.backend.usecase.types.GetUsersViewsRepository
 import org.jetbrains.exposed.sql.Database
 
 fun notificationDependenciesFactory(
     db: Database,
-    authRepository: AuthRepository = authRepository(db),
-    getMeetingsViewsRepository: GetMeetingsViewsRepository = getMeetingsViewsRepository(db),
-    getUsersViewsRepository: GetUsersViewsRepository = getUserViewsRepository(db)
+    authRepository: AuthRepository = authRepository(db)
 ): NotificationsDependencies = NotificationsDependencies(
-    listNotificationsRepository = UsecaseListNotificationsRepository(
-        usecase = GetNotificationsUsecase(
-            db = db,
-            authRepository = authRepository,
-            meetingsRepository = getMeetingsViewsRepository,
-            usersRepository = getUsersViewsRepository
-        )
-    ),
+    listNotificationsRepository = listNotificationRepository(db),
     readNotificationsRepository = UsecaseReadNotificationsRepository(
         usecase = ReadNotificationsUsecase(
             authRepository = authRepository,
