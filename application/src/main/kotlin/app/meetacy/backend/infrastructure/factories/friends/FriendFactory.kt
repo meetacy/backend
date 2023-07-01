9d8dd977679995fd0be1a2d28ac1facec812dc2a
bpavuk <boopentiy@gmail.com>
2023-07-01 11:22:15 +0300

    WIP: beaten-up locate friends usecase

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/friends/FriendFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/friends/FriendFactory.kt
@@ -1,30 +1,12 @@
 package app.meetacy.backend.infrastructure.factories.friends
 
-import app.meetacy.backend.database.integration.location.stream.DatabaseFriendsLocationStreamingStorage
 import app.meetacy.backend.endpoint.friends.FriendsDependencies
-import app.meetacy.backend.endpoint.friends.location.FriendsLocationDependencies
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
-import app.meetacy.backend.infrastructure.factories.users.getUserViewsRepository
-import app.meetacy.backend.usecase.integration.friends.location.stream.UsecaseStreamLocationRepository
-import app.meetacy.backend.usecase.location.stream.FriendsLocationStreamingUsecase
-import app.meetacy.backend.usecase.types.AuthRepository
-import app.meetacy.backend.usecase.types.GetUsersViewsRepository
 import org.jetbrains.exposed.sql.Database
 
 fun friendDependenciesFactory(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db),
-    getUsersViewsRepository: GetUsersViewsRepository = getUserViewsRepository(db)
+    db: Database
 ): FriendsDependencies = FriendsDependencies(
-    friendsLocationDependencies = FriendsLocationDependencies(
-        streamLocationRepository = UsecaseStreamLocationRepository(
-            usecase = FriendsLocationStreamingUsecase(
-                authRepository = authRepository,
-                storage = DatabaseFriendsLocationStreamingStorage(db),
-                usersViewsRepository = getUsersViewsRepository
-            )
-        )
-    ),
+    friendsLocationDependencies = friendLocationDependenciesBuilder(db),
     addFriendRepository = addFriendRepository(db),
     listFriendsRepository = listFriendsRepository(db),
     deleteFriendRepository = deleteFriendRepositoryBuilder(db)
