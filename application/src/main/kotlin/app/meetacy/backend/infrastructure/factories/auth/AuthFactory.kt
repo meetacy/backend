76c9a653576b8a940d9ae53d7c6c4bde9a8a0d53
bpavuk <boopentiy@gmail.com>
2023-07-01 11:34:59 +0300

    WIP: beaten up the token generator repo

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/AuthFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/auth/AuthFactory.kt
@@ -1,23 +1,13 @@
 package app.meetacy.backend.infrastructure.factories.auth
 
-import app.meetacy.backend.database.integration.tokenGenerator.DatabaseGenerateTokenStorage
 import app.meetacy.backend.endpoint.auth.AuthDependencies
-import app.meetacy.backend.hash.integration.DefaultHashGenerator
 import app.meetacy.backend.infrastructure.factories.auth.email.emailDependencies
-import app.meetacy.backend.usecase.auth.GenerateTokenUsecase
-import app.meetacy.backend.usecase.integration.auth.UsecaseTokenGenerateRepository
-import app.meetacy.backend.utf8.integration.DefaultUtf8Checker
+import app.meetacy.backend.infrastructure.factories.auth.tokenGenerator.tokenGenerateRepository
 import org.jetbrains.exposed.sql.Database
 
 fun authDependenciesFactory(
     db: Database
 ): AuthDependencies = AuthDependencies(
-        emailDependencies = emailDependencies(db),
-        tokenGenerateRepository = UsecaseTokenGenerateRepository(
-            usecase = GenerateTokenUsecase(
-                storage = DatabaseGenerateTokenStorage(DefaultHashGenerator, db),
-                tokenGenerator = DefaultHashGenerator,
-                utf8Checker = DefaultUtf8Checker
-            )
-        )
-    )
+    emailDependencies = emailDependencies(db),
+    tokenGenerateRepository = tokenGenerateRepository(db)
+)
