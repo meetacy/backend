e55ce01defafaf8ca983f42d7c76d8cf153f2ffb
bpavuk <boopentiy@gmail.com>
2023-07-01 13:00:37 +0300

    WIP: beaten up the updates dependencies

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/updates/UpdatesFuckery.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/updates/UpdatesFuckery.kt
@@ -1,27 +1,11 @@
 package app.meetacy.backend.infrastructure.factories.updates
 
-import app.meetacy.backend.database.integration.updates.stream.StreamUpdatesUsecase
 import app.meetacy.backend.endpoint.updates.UpdatesDependencies
-import app.meetacy.backend.infrastructure.factories.auth.authRepository
-import app.meetacy.backend.infrastructure.factories.notifications.list.getNotificationViewsRepository
-import app.meetacy.backend.infrastructure.factories.notifications.add.updatesMiddlewareBuilder
-import app.meetacy.backend.usecase.integration.updates.stream.UsecaseStreamUpdatesRepository
-import app.meetacy.backend.usecase.types.AuthRepository
-import app.meetacy.backend.usecase.types.GetNotificationsViewsRepository
-import app.meetacy.backend.usecase.updates.stream.UpdatesMiddleware
+import app.meetacy.backend.infrastructure.factories.updates.stream.streamUpdatesRepository
 import org.jetbrains.exposed.sql.Database
 
 fun updatesDependenciesFactory(
-    db: Database,
-    authRepository: AuthRepository = authRepository(db),
-    getNotificationsViewsRepository: GetNotificationsViewsRepository = getNotificationViewsRepository(db),
-    updatesMiddleware: UpdatesMiddleware = updatesMiddlewareBuilder(db)
+    db: Database
 ): UpdatesDependencies = UpdatesDependencies(
-    streamUpdatesRepository = UsecaseStreamUpdatesRepository(
-        usecase = StreamUpdatesUsecase(
-            auth = authRepository,
-            notificationsRepository = getNotificationsViewsRepository,
-            updatesMiddleware = updatesMiddleware
-        )
-    )
+    streamUpdatesRepository = streamUpdatesRepository(db)
 )
