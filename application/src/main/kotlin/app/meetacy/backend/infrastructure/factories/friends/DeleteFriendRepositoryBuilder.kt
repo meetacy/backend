6753dae54218d7538f151d5b3bb967bee55cd75c
bpavuk <boopentiy@gmail.com>
2023-07-01 11:19:07 +0300

    WIP: beaten-up delete friends usecase

---
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/friends/DeleteFriendRepositoryBuilder.kt
@@ -0,0 +1,23 @@
+package app.meetacy.backend.infrastructure.factories.friends
+
+import app.meetacy.backend.database.integration.friends.delete.DatabaseDeleteFriendStorage
+import app.meetacy.backend.endpoint.friends.delete.DeleteFriendRepository
+import app.meetacy.backend.infrastructure.factories.auth.authRepository
+import app.meetacy.backend.infrastructure.factories.users.getUserViewsRepository
+import app.meetacy.backend.usecase.friends.delete.DeleteFriendUsecase
+import app.meetacy.backend.usecase.integration.friends.delete.UsecaseDeleteFriendRepository
+import app.meetacy.backend.usecase.types.AuthRepository
+import app.meetacy.backend.usecase.types.GetUsersViewsRepository
+import org.jetbrains.exposed.sql.Database
+
+fun deleteFriendRepositoryBuilder(
+    db: Database,
+    authRepository: AuthRepository = authRepository(db),
+    getUsersViewsRepository: GetUsersViewsRepository = getUserViewsRepository(db)
+): DeleteFriendRepository = UsecaseDeleteFriendRepository(
+    usecase = DeleteFriendUsecase(
+        authRepository = authRepository,
+        getUsersViewsRepository = getUsersViewsRepository,
+        storage = DatabaseDeleteFriendStorage(db)
+    )
+)
--- application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/friends/FriendFactory.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/factories/friends/FriendFactory.kt
@@ -1,13 +1,10 @@
 package app.meetacy.backend.infrastructure.factories.friends
 
-import app.meetacy.backend.database.integration.friends.delete.DatabaseDeleteFriendStorage
 import app.meetacy.backend.database.integration.location.stream.DatabaseFriendsLocationStreamingStorage
 import app.meetacy.backend.endpoint.friends.FriendsDependencies
 import app.meetacy.backend.endpoint.friends.location.FriendsLocationDependencies
 import app.meetacy.backend.infrastructure.factories.auth.authRepository
 import app.meetacy.backend.infrastructure.factories.users.getUserViewsRepository
-import app.meetacy.backend.usecase.friends.delete.DeleteFriendUsecase
-import app.meetacy.backend.usecase.integration.friends.delete.UsecaseDeleteFriendRepository
 import app.meetacy.backend.usecase.integration.friends.location.stream.UsecaseStreamLocationRepository
 import app.meetacy.backend.usecase.location.stream.FriendsLocationStreamingUsecase
 import app.meetacy.backend.usecase.types.AuthRepository
@@ -30,11 +27,5 @@ fun friendDependenciesFactory(
     ),
     addFriendRepository = addFriendRepository(db),
     listFriendsRepository = listFriendsRepository(db),
-    deleteFriendRepository = UsecaseDeleteFriendRepository(
-        usecase = DeleteFriendUsecase(
-            authRepository = authRepository,
-            getUsersViewsRepository = getUsersViewsRepository,
-            storage = DatabaseDeleteFriendStorage(db)
-        )
-    )
+    deleteFriendRepository = deleteFriendRepositoryBuilder(db)
 )
