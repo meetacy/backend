92a2dcbd9da3abe981db5125caace269c65501ca
bpavuk <boopentiy@gmail.com>
2023-07-05 23:39:18 +0300

    finish: added suppressing

--- application/src/main/kotlin/app/meetacy/backend/infrastructure/integrations/notifications/list/DI.kt
+++ application/src/main/kotlin/app/meetacy/backend/infrastructure/integrations/notifications/list/DI.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 package app.meetacy.backend.infrastructure.integrations.notifications.list
 
 import app.meetacy.backend.database.integration.notifications.DatabaseGetNotificationStorage
