632319707fa940c969b948021f625dc853e701c9
bpavuk <boopentiy@gmail.com>
2023-07-06 16:54:23 +0300

    wip: linked dependencies

--- invitation/database/integrations/build.gradle.kts
+++ invitation/database/integrations/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     id(Deps.Plugins.Configuration.Kotlin.Jvm)
 }
+
+dependencies {
+    api(project(Deps.Projects.Invitation.Usecase))
+    api(project(Deps.Projects.Invitation.Database))
+}
--- invitation/usecase/integrations/build.gradle.kts
+++ invitation/usecase/integrations/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id(Deps.Plugins.Configuration.Kotlin.Jvm)
 }
+
+// usecase and endpoints dependencies
+dependencies {
+    implementation(project(Deps.Projects.Usecase))
+    implementation(project(Deps.Projects.Endpoints))
+}
