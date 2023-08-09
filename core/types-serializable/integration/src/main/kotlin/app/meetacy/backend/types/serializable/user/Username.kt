7c2eb71265623a6126184b66052a02cb09857d2b
y9Kap <wotblitz6allima4ok@gmail.com>
2023-08-09 07:59:08 +0300

    WIP(beat-up): rename files and classes

--- core/types-serializable/integration/src/main/kotlin/app/meetacy/backend/types/serializable/user/Username.kt
+++ core/types-serializable/integration/src/main/kotlin/app/meetacy/backend/types/serializable/user/Username.kt
@@ -3,6 +3,6 @@ package app.meetacy.backend.types.serializable.user
 import app.meetacy.backend.types.user.Username
 import app.meetacy.backend.types.serializable.user.Username as UsernameSerializable
 
-fun Username.serializable(): UsernameSerializable = UsernameSerializable(string)
-
 fun UsernameSerializable.type(): Username = Username.parse(string)
+
+fun Username.serializable(): UsernameSerializable = UsernameSerializable(string)
