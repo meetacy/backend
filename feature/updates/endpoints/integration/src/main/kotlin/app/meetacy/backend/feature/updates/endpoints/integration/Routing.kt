6cf050c3c59da3c3622b2d1af3f5bff82ec44fb8
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-08-31 09:28:53 +0300

    WIP: refactor: fixed updates endpoints integration

--- feature/updates/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/updates/endpoints/integration/Routing.kt
+++ feature/updates/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/updates/endpoints/integration/Routing.kt
@@ -1,5 +1,6 @@
 package app.meetacy.backend.feature.updates.endpoints.integration
 
+import app.meetacy.backend.feature.updates.endpoints.integration.stream.streamUpdates
 import app.meetacy.backend.feature.updates.endpoints.updates.stream.StreamUpdatesRepository
 import app.meetacy.backend.feature.updates.usecase.updates.stream.StreamUpdatesUsecase
 import app.meetacy.backend.types.serializable.access.AccessIdentity
@@ -13,20 +14,5 @@ import io.ktor.server.routing.*
 import kotlinx.coroutines.flow.map
 
 fun Route.updates() {
-    val repository = object : StreamUpdatesRepository {
-        val usecase: StreamUpdatesUsecase by di.getting
-        override suspend fun flow(
-            token: AccessIdentity,
-            fromId: UpdateId?
-        ): StreamUpdatesRepository.Result {
-            return when (
-                val result = usecase.flow(token.type(), fromId?.type())
-            ) {
-                is StreamUpdatesUsecase.Result.Ready -> StreamUpdatesRepository.Result.Ready(
-                    flow = result.flow.map(UpdateView::mapToEndpoint)
-                )
-                is StreamUpdatesUsecase.Result.TokenInvalid -> StreamUpdatesRepository.Result.TokenInvalid
-            }
-        }
-    }
+    streamUpdates()
 }
---
+++ feature/updates/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/updates/endpoints/integration/stream/Routing.kt
@@ -0,0 +1,36 @@
+package app.meetacy.backend.feature.updates.endpoints.integration.stream
+
+import app.meetacy.backend.feature.updates.endpoints.updates.stream.StreamUpdatesRepository
+import app.meetacy.backend.feature.updates.endpoints.updates.stream.streamUpdates
+import app.meetacy.backend.feature.updates.usecase.updates.stream.StreamUpdatesUsecase
+import app.meetacy.backend.types.serializable.access.AccessIdentity
+import app.meetacy.backend.types.serializable.access.type
+import app.meetacy.backend.types.serializable.update.UpdateId
+import app.meetacy.backend.types.serializable.update.mapToEndpoint
+import app.meetacy.backend.types.serializable.update.type
+import app.meetacy.backend.types.update.UpdateView
+import app.meetacy.di.global.di
+import io.ktor.server.routing.*
+import kotlinx.coroutines.flow.map
+
+internal fun Route.streamUpdates() {
+    val streamUpdatesUsecase: StreamUpdatesUsecase by di.getting
+
+    val repository = object : StreamUpdatesRepository {
+        override suspend fun flow(
+            token: AccessIdentity,
+            fromId: UpdateId?
+        ): StreamUpdatesRepository.Result {
+            return when (
+                val result = streamUpdatesUsecase.flow(token.type(), fromId?.type())
+            ) {
+                is StreamUpdatesUsecase.Result.Ready -> StreamUpdatesRepository.Result.Ready(
+                    flow = result.flow.map(UpdateView::mapToEndpoint)
+                )
+                is StreamUpdatesUsecase.Result.TokenInvalid -> StreamUpdatesRepository.Result.TokenInvalid
+            }
+        }
+    }
+
+    streamUpdates(repository)
+}
