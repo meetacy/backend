5303db874f09484f9f1f4e589d6ad4a2dd5fd10e
bpavuk <boopentiy@gmail.com>
2023-09-03 23:47:22 +0300

    wip: optimized imports

--- feature/updates/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/updates/endpoints/integration/Routing.kt
+++ feature/updates/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/updates/endpoints/integration/Routing.kt
@@ -1,17 +1,8 @@
 package app.meetacy.backend.feature.updates.endpoints.integration
 
 import app.meetacy.backend.feature.updates.endpoints.integration.stream.streamUpdates
-import app.meetacy.backend.feature.updates.endpoints.updates.stream.StreamUpdatesRepository
-import app.meetacy.backend.feature.updates.usecase.updates.stream.StreamUpdatesUsecase
-import app.meetacy.backend.types.serializable.access.AccessIdentity
-import app.meetacy.backend.types.serializable.access.type
-import app.meetacy.backend.types.serializable.update.UpdateId
-import app.meetacy.backend.types.serializable.update.mapToEndpoint
-import app.meetacy.backend.types.serializable.update.type
-import app.meetacy.backend.types.update.UpdateView
 import app.meetacy.di.DI
 import io.ktor.server.routing.*
-import kotlinx.coroutines.flow.map
 
 fun Route.updates(di: DI) = route("/updates") {
     streamUpdates(di)
