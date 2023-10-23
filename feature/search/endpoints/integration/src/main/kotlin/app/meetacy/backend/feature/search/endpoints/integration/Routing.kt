e6fa533fb979e1b8c223173ce5394e0ecaff5b18
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-10-23 15:00:37 +0300

    WIP: feat(#143): implementation of search endpoint

---
+++ feature/search/endpoints/integration/src/main/kotlin/app/meetacy/backend/feature/search/endpoints/integration/Routing.kt
@@ -0,0 +1,8 @@
+package app.meetacy.backend.feature.search.endpoints.integration
+
+import io.ktor.server.routing.*
+import app.meetacy.di.DI
+
+fun Route.search(di: DI) = route("/search") {
+    
+}
