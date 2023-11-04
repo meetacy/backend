4134cf03457d2a817a21fd431dbde235357caccb
y9Kap <wotblitz6allima4ok@gmail.com>
2023-11-04 12:05:54 +0300

    feat(#143-search-endpoints): add test search

--- application/src/test/kotlin/TestSearch.kt
+++ application/src/test/kotlin/TestSearch.kt
@@ -1,11 +1,16 @@
 import app.meetacy.sdk.types.location.Location
+import app.meetacy.sdk.types.search.SearchItem
 import kotlin.test.Test
 
 class TestSearch {
     @Test
-    fun `test search`() = runTestServer {
+    fun `test meeting search`() = runTestServer {
         val self = generateTestAccount()
-        self.meetings.createTestMeeting("test")
-        self.api.search(Location.NullIsland, "test")
+        self.meetings.createTestMeeting("testSearch")
+        val result = self.api.search(Location.NullIsland, "testSearch")
+        val data = result.first().data
+        require(data is SearchItem.Meeting)
+        val meeting = data.meeting
+        require(meeting.title == "testSearch")
     }
 }
