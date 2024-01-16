a9f923312966075b7c93804fdd35ca73e5f2254a
y9Kap <wotblitz6allima4ok@gmail.com>
2024-01-17 00:37:13 +0300

    fix(#90-change-http-request-type): pagingId most be nullable

--- core/endpoints/src/main/kotlin/app/meetacy/backend/core/endpoints/PagingId.kt
+++ core/endpoints/src/main/kotlin/app/meetacy/backend/core/endpoints/PagingId.kt
@@ -1,13 +1,12 @@
 package app.meetacy.backend.core.endpoints
 
 import app.meetacy.backend.types.paging.serializable.PagingId
-import io.ktor.server.application.*
-import kotlinx.serialization.SerializationException
 import app.meetacy.backend.types.serializable.serialization
+import io.ktor.server.application.*
 
-fun ApplicationCall.pagingId(): PagingId = serialization {
+fun ApplicationCall.pagingId(): PagingId? = serialization {
     val pagingId = parameters["pagingId"]
     return if (pagingId == null) {
-        throw SerializationException("Bad request. Illegal input: param 'pagingId' is required for type with serial name, but it was missing at path: $")
+        null
     } else PagingId(pagingId)
 }
