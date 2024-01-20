2c36edfe6e274c8aacb366ca4916ac05b2b8c2b2
y9Kap <wotblitz6allima4ok@gmail.com>
2024-01-20 13:56:09 +0300

    feat(#90-change-http-request-type): moved serialization trycatch

--- core/endpoints/build.gradle.kts
+++ core/endpoints/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     api(projects.libs.ktorExtensions)
     api(projects.core.types.serializable)
     api(projects.libs.paging.serializable)
-    api(projects.core.types.serializable.integration)
     api(libs.ktor.server.core)
     api(libs.kotlinx.serialization.json)
 
