27bda37b08f67f9129dedf5b7b554d2c4cab83ef
y9Kap <wotblitz6allima4ok@gmail.com>
2024-03-04 21:51:07 +0300

    feat(#190-change-getUser-request-type): getUser request type was changed

--- feature/users/endpoints/src/main/kotlin/app/meetacy/backend/feature/users/endpoints/get/Routing.kt
+++ feature/users/endpoints/src/main/kotlin/app/meetacy/backend/feature/users/endpoints/get/Routing.kt
@@ -1,27 +1,19 @@
 package app.meetacy.backend.feature.users.endpoints.get
 
 import app.meetacy.backend.core.endpoints.accessIdentity
+import app.meetacy.backend.core.endpoints.userId
 import app.meetacy.backend.endpoint.ktor.Failure
 import app.meetacy.backend.endpoint.ktor.respondFailure
 import app.meetacy.backend.endpoint.ktor.respondSuccess
 import app.meetacy.backend.types.serializable.access.AccessIdentity
 import app.meetacy.backend.types.serializable.users.User
 import app.meetacy.backend.types.serializable.users.UserIdentity
-import com.typesafe.config.Optional
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 
 interface UserRepository {
     suspend fun getUser(id: UserIdentity?, token: AccessIdentity): GetUserResult
 }
-
-@Serializable
-@Optional
-data class GetUserParams(
-    val id: UserIdentity? = null
-)
 
 sealed interface GetUserResult {
     data object InvalidIdentity : GetUserResult
@@ -29,8 +21,8 @@ sealed interface GetUserResult {
     class Success(val user: User) : GetUserResult
 }
 
-fun Route.getUser(provider: UserRepository) = post("/get") {
-    val id = call.receive<GetUserParams>().id
+fun Route.getUser(provider: UserRepository) = get("/get") {
+    val id = call.userId()
     val token = call.accessIdentity()
     when (val result = provider.getUser(id, token)) {
         is GetUserResult.Success -> call.respondSuccess(result.user)
